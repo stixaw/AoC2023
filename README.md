11eb0866533e0eb5330ecbced1b188af262fa4b5
Angel Williams <stixaw@gmail.com>
2023-12-04 09:50:42 -0700

    cleaned up some files added some details

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AoC2023
+
 advent of calendar 2023
+
+run npm install
+
+## To run a Day
--- aoc2022/d1/part1.js
+++ aoc2022/d1/part1.js
@@ -1,23 +1,3 @@
 import input from "./input.js"
 
-
-const elementsArray = inputString.split('\n')
-
-let totalCalibrationValue = 0
-
-elementsArray.forEach(element => {
-    //using regex
-    const firstDigit = element.match(/\d/)
-    const lastDigit = element.match(/\d(?=\D*$)/)
-
-    const calibrationValue = firstDigit && lastDigit ? parseInt(`${firstDigit[0]}${lastDigit[0]}`, 10) : null
-
-    totalCalibrationValue += calibrationValue
-
-    console.log(`Element: ${element}`);
-    console.log(`Calibration Value: ${calibrationValue !== null ? calibrationValue : 'No digit found'}`)
-    console.log('---------------------')
-})
-
-console.log(`Total Calibration Value: ${totalCalibrationValue}`)
-return totalCalibrationValue
+// i lost my solution to puuzzle 1 from 2022
