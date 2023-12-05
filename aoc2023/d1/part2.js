92ddcbcdc04b88da9eadc766e09c8d8c622e8336
Angel Williams <stixaw@gmail.com>
2023-12-04 17:06:10 -0700

    cleanup of code

--- aoc2023/d1/part2.js
+++ aoc2023/d1/part2.js
@@ -1,27 +1,49 @@
 import input from "./input.js"
 
+const elementsArray = input.trim().split('\n')
 
-const elementsArray = input.split('\n')
-
-
-const spelledOutDigits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].reduce((acc, digit, index) => {
-  acc[digit] = index + 1
-  return acc
-}, {})
-
+const spelledOutDigits = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9
+}
+console.log(spelledOutDigits)
 
 let totalCalibrationValue = 0
 
+// Function to find all matches including overlapping ones
+function findAllMatches(string, regex) {
+  let match
+  let matches = []
+
+  while (match = regex.exec(string)) {
+    matches.push(match[0])
+    regex.lastIndex = match.index + 1
+  }
+  return matches
+}
+
 elementsArray.forEach(element => {
-  const digitMatches = element.match(new RegExp(`(\\d|${Object.keys(spelledOutDigits).join("|")})`, "gi"));
+
+  const digitMatches = findAllMatches(element, new RegExp(`(\\d|${Object.keys(spelledOutDigits).join("|")})`, "gi"))
 
   if (digitMatches && digitMatches.length >= 2) {
-    const firstDigit = spelledOutDigits[digitMatches[0].toLowerCase()] || parseInt(digitMatches[0]);
-    const lastDigit = spelledOutDigits[digitMatches[digitMatches.length - 1].toLowerCase()] || parseInt(digitMatches[digitMatches.length - 1]);
+    const firstDigit = spelledOutDigits[digitMatches[0].toLowerCase()] || parseInt(digitMatches[0])
 
-    const calibrationValue = firstDigit * 10 + lastDigit;
-    totalCalibrationValue += calibrationValue;
+    const lastDigit = spelledOutDigits[digitMatches[digitMatches.length - 1].toLowerCase()] || parseInt(digitMatches[digitMatches.length - 1])
+
+
+    const calibrationValue = firstDigit * 10 + lastDigit
+
+    totalCalibrationValue = totalCalibrationValue + calibrationValue
+    console.log(`Element: ${element}, digits: ${digitMatches}, calibrationValue: ${calibrationValue}, totalCalibrationValue: ${totalCalibrationValue}`)
   }
-});
+})
 
 console.log(`Total Calibration Value: ${totalCalibrationValue}`)
